c761af7011c2e4d0da4ed72e12e0d6fc60bdb84d
adlermo <adlerorteney@gmail.com>
2023-01-07 10:15:28 -0300

    Prep1 implemented and tested

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
     }
 }
