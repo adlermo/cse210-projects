2855ca50110ec1a4de813742a5ff6fcbada086f0
adlermo <adlerorteney@gmail.com>
2023-01-12 11:12:57 -0300

    Finished Task

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int input;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Integer number: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0) numbers.Add(input);
+        } while (input != 0);
+
+        Console.WriteLine($"\nThe sum is {numbers.Sum()}");
+        Console.WriteLine($"The average is {numbers.Average()}");
+        Console.WriteLine($"The largest number is {numbers.Max()}");
+        Console.WriteLine($"The smallest positive is {numbers.Min(n => n > 0 ? n : null)}");
+        numbers.Sort();
+        Console.WriteLine($"The sorted list is: [ {String.Join(", ", numbers)} ]\n");
     }
 }
