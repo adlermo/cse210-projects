1bb75d9acb8cc3fd8ace099b5bc6ec7ab0a6b367
Adler Mesquita Orteney <adlerorteney@gmail.com>
2023-01-16 09:48:07 -0300

    Introducing classes to Prepare02

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,11 @@
+public class Job {
+    string _company;
+    string _jobTitle;
+    int _startYear;
+    int _endYear;
+
+    Display() {
+        Console.WriteLine($"{_jobTitle}")
+    }
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer"
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer"
+
+        job1.Display()
+        job2.Display()
     }
 }
