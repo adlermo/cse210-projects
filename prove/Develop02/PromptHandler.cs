1d1b9868d29019ba53125c4da5c0a0c06a29b597
adlermo <adlerorteney@gmail.com>
2023-01-20 17:40:13 -0300

    Handling duplicates and adding prompts

--- prove/Develop02/PromptHandler.cs
+++ prove/Develop02/PromptHandler.cs
@@ -10,15 +10,37 @@ public class PromptHandler
         this._promptList.Add(new Prompt("How did I see the hand of the Lord in my life today?"));
         this._promptList.Add(new Prompt("What was the strongest emotion I felt today?"));
         this._promptList.Add(new Prompt("If I had one thing I could do over today, what would it be?"));
+        this._promptList.Add(new Prompt("Write an advice to your older self in 5 years:"));
+
+        this._promptList.Add(new Prompt("How many cups of water did you drink today?"));
     }
 
     public string GetRandomPrompt()
     {
-        int index = new Random().Next(1, 5);
-        Prompt p = this._promptList[index];
-        this._promptHistory.Add(p);
+        Prompt p = handleDuplicated();
 
         return p.GetPrompt();
+    }
+
+    Prompt handleDuplicated()
+    {
+        // When the user has used all Prompts we restart the list
+        if (this._promptList.Count().Equals(0)) { RecycleHistory(); }
+
+        int index = new Random().Next(1, this._promptList.Count());
+        Prompt prompt = this._promptList[index];
+
+        this._promptList.Remove(prompt);
+        this._promptHistory.Add(prompt);
+
+        return prompt;
+    }
+
+    void RecycleHistory()
+    {
+        this._promptList = this._promptHistory;
+
+        this._promptHistory.Clear();
     }
 
 }
